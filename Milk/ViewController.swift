434b3d18c0d97fc6e0ba48232ea980da54bf22b8
Herman Lönnqvist <lonnqvistherman@gmail.com>
2020-07-07 14:54:26 +0200

    Initial Commit

---
+++ Milk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Milk
+//
+//  Created by Herman Lönnqvist on 2020-07-07.
+//  Copyright © 2020 Herman Lonnqvist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
